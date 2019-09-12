ecd5cb21b85834d2c2afc283c5b211adf3251780
LukeStro47 <lukestrong47@gmail.com>
2019-09-12 16:32:44 -0400

    Signed-off-by: LukeStro47 <lukestrong47@gmail.com>

---
+++ Login/js/auth.js
@@ -0,0 +1,35 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB0Om-pggrXGPrdTDmb-TLF8oefyWHVth4",
+    authDomain: "the-dave-initiative.firebaseapp.com",
+    databaseURL: "https://the-dave-initiative.firebaseio.com",
+    projectId: "the-dave-initiative",
+    storageBucket: "",
+    messagingSenderId: "637804637403",
+    appId: "1:637804637403:web:dec14656b88e73798fb0fe"
+};
+firebase.initializeApp(firebaseConfig);
+initApp();
+function login() {
+    firebase.auth().signInWithEmailAndPassword(document.getElementById('email').value, document.getElementById('password').value).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage);
+    });
+}
+function initApp() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user && document.getElementById('unique') != null) {
+            location.replace("../../dashboard.html");
+        }
+    });
+}
+function check() {
+    setTimeout(function() {
+        if(firebase.auth().currentUser == null) {
+            window.close();
+        }
+    }, 1000);
+}
+function callDave() {
+    
+}
